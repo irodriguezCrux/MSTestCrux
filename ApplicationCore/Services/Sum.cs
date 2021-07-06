465c2237b0e8b94469d4386aa43423f8f68c42cf
irodriguezCrux <irodriguez@cruxconsultores.com>
2021-07-05 23:28:36 -0600

    Cuarto commit

---
+++ ApplicationCore/Services/Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class Sum
+    {
+        /// <summary>
+        /// Suma de valores
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int SumValues(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ UnitTest/Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumValues()
+        {
+            //AAA
+
+            //Arrange
+            ApplicationCore.Services.Sum suma = new ApplicationCore.Services.Sum();
+
+            //Act
+            var result = suma.SumValues(5, 2);
+            //
+
+            //Assert
+            Assert.AreEqual(5, result);
+
+        }
+    }
+} 
